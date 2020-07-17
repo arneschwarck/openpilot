f0b246776b8615ec60c6896d43d6d46f512e3b09
alfhern <hz.alfonso@gmail.com>
2020-07-17 15:23:38 +0200

    Hands on the wheel

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -31,6 +31,7 @@ def manager_init():
     ("IsRHD", "0"),
     ("IsMetric", "0"),
     ("RecordFront", "0"),
+    ("HandsOnWheelMonitoring", "0"),
     ("HasAcceptedTerms", "0"),
     ("HasCompletedSetup", "0"),
     ("IsUploadRawEnabled", "1"),
@@ -48,6 +49,10 @@ def manager_init():
   for k, v in default_params:
     if params.get(k) is None:
       params.put(k, v)
+
+  # parameters set by Enviroment Varables
+  if os.getenv("HANDSMONITORING") is not None:
+    params.put("HandsOnWheelMonitoring", str(int(os.getenv("HANDSMONITORING"))))
 
   # is this dashcam?
   if os.getenv("PASSIVE") is not None:
