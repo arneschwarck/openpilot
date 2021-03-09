21bba8eb5d3ce443c2fdad351ec4cb703a918b40
alfhern <hz.alfonso@gmail.com>
2021-03-09 13:59:08 +0100

    Speed Limit Controller to make use of Live Map Data

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -204,7 +204,7 @@ class Planner():
       self.v_cruise = max(self.v_cruise, 0.)
       # update speed limit solution calculation.
       if not travis:
-        self.speed_limit_controller.update(enabled, self.v_acc_start, self.a_acc_start, sm['carState'],
+        self.speed_limit_controller.update(enabled, self.v_acc_start, self.a_acc_start, sm,
                                          v_cruise_setpoint, accel_limits_turns, jerk_limits, self.events)
       # update turn speed solution calculation.
         self.turn_speed_controller.update(enabled, self.v_acc_start, self.a_acc_start, sm, accel_limits_turns,
@@ -289,6 +289,7 @@ class Planner():
 
       longitudinalPlan.decelForTurnDEPRECATED = bool(self.turn_controller.is_active)
       longitudinalPlan.speedLimitControlState = self.speed_limit_controller.state
+      longitudinalPlan.speedLimit = float(self.speed_limit_controller.speed_limit)
     longitudinalPlan.eventsDEPRECATED = self.events.to_msg()
 
     longitudinalPlan.processingDelay = (plan_send.logMonoTime / 1e9) - sm.rcv_time['radarState']
