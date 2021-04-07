f198fbec74ae143464d2e49b6e0075abbbf28ecf
Arne Schwarck <arneschwarck@gmail.com>
2021-04-07 12:38:45 +0200

    fix undefined name 'vectors'

--- selfdrive/mapd/lib/test_NodesData.py
+++ selfdrive/mapd/lib/test_NodesData.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 from numpy.testing import assert_array_almost_equal
 from .mock_data import MockRoad
-#from .NodesData import vectors
+from .NodesData import vectors
 
 
 class TestNodesData(unittest.TestCase):
