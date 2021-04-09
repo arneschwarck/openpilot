ead73c225b90c6c69e638a91339d68f5184831ab
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2021-04-09 15:39:46 -0700

    fix

--- selfdrive/ui/qt/home.cc
+++ selfdrive/ui/qt/home.cc
@@ -52,15 +52,15 @@ HomeWindow::HomeWindow(QWidget* parent) : QWidget(parent) {
 void HomeWindow::mousePressEvent(QMouseEvent* e) {
   UIState* ui_state = &glWindow->ui_state;
   if (GLWindow::ui_state.scene.driver_view) {
-    if (scene.controls_state.getSpeedLimit() > 0.0
-          && touch_y >= ui_state->scene.ui_speed_sgn_y - speed_sgn_touch_pad
-          && touch_y < ui_state->scene.ui_speed_sgn_y + 2 * speed_sgn_r + speed_sgn_touch_pad
-          && touch_x >= ui_state->scene.ui_speed_sgn_x - speed_sgn_touch_pad
-          && touch_x < ui_state->scene.ui_speed_sgn_x + 2 * speed_sgn_r + speed_sgn_touch_pad) {
+    if (ui_state->scene.controls_state.getSpeedLimit() > 0.0
+          && e->x() >= ui_state->scene.ui_speed_sgn_y - speed_sgn_touch_pad
+          && e->x() < ui_state->scene.ui_speed_sgn_y + 2 * speed_sgn_r + speed_sgn_touch_pad
+          && e->y() >= ui_state->scene.ui_speed_sgn_x - speed_sgn_touch_pad
+          && e->y() < ui_state->scene.ui_speed_sgn_x + 2 * speed_sgn_r + speed_sgn_touch_pad) {
         // If touching the speed limit sign area when visible
         ui_state->last_speed_limit_sign_tap = seconds_since_boot();
         ui_state->speed_limit_control_enabled = !ui_state->speed_limit_control_enabled;
-        Params().putBool(ui_state->speed_limit_control_enabled, "SpeedLimitControl");
+        Params().putBool("SpeedLimitControl", false );
       } else {
         ui_state->sidebar_collapsed = !ui_state->sidebar_collapsed
       }
@@ -86,7 +86,7 @@ void HomeWindow::mousePressEvent(QMouseEvent* e) {
     // If touching the speed limit sign area when visible
     ui_state->last_speed_limit_sign_tap = seconds_since_boot();
     ui_state->speed_limit_control_enabled = !ui_state->speed_limit_control_enabled;
-    Params().putBool(ui_state->speed_limit_control_enabled, "SpeedLimitControl");
+    Params().putBool("SpeedLimitControl", false);
   }
 
   // Handle sidebar collapsing
