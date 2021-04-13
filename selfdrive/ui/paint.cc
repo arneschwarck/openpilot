3a3d19a877d41a9530b62d3429d72fa9c31f7d04
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2021-04-13 12:03:32 -0700

    move it a bit.

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -673,13 +673,13 @@ static void bb_ui_draw_measures_right(UIState *s, int bb_x, int bb_y, int bb_w )
 //BB END: functions added for the display of various items
 
 static void bb_ui_draw_UI(UIState *s) {
-  const int bb_dml_w = 180;
-  const int bb_dml_x = (s->viz_rect.x + (bdr_s));
-  const int bb_dml_y = (s->viz_rect.y + (bdr_s)) + 220;
+  const int bb_dml_w = 184;
+  const int bb_dml_x = (s->viz_rect.x + (bdr_s*2));
+  const int bb_dml_y = (s->viz_rect.y + (bdr_s*1.5)) + 220;
 
-  const int bb_dmr_w = 180;
-  const int bb_dmr_x = s->viz_rect.x + s->viz_rect.w - bb_dmr_w - (bdr_s);
-  const int bb_dmr_y = (s->viz_rect.y + (bdr_s)) + 220;
+  const int bb_dmr_w = 184;
+  const int bb_dmr_x = s->viz_rect.x + s->viz_rect.w - bb_dmr_w - (bdr_s*2);
+  const int bb_dmr_y = (s->viz_rect.y + (bdr_s*1.5)) + 220;
 
   bb_ui_draw_measures_right(s, bb_dml_x, bb_dml_y, bb_dml_w);
   bb_ui_draw_measures_left(s, bb_dmr_x, bb_dmr_y-20, bb_dmr_w);
