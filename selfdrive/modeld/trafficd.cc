506c3d82320746fcdec1ee8f33dfa79e4b211b2d
Comma Device <device@comma.ai>
2021-04-17 00:08:52 -0500

    hmm

--- selfdrive/modeld/trafficd.cc
+++ selfdrive/modeld/trafficd.cc
@@ -127,7 +127,7 @@ int main(){
   std::vector<float> output;
   output.resize(numLabels);
 //  RunModel *model = new DefaultRunModel("../../models/traffic_model.dlc", output, numLabels, USE_GPU_RUNTIME);
-  std::unique_ptr<RunModel> model = std::make_unique<ThneedModel>("../../models/traffic_model.thneed", output[0], numLabels, USE_GPU_RUNTIME);
+  std::unique_ptr<RunModel> model = std::make_unique<ThneedModel>("../../models/traffic_model.thneed", &output[0], numLabels, USE_GPU_RUNTIME);
 
   cl_device_id device_id = cl_get_device_id(CL_DEVICE_TYPE_DEFAULT);
   cl_context context = CL_CHECK_ERR(clCreateContext(NULL, 1, &device_id, NULL, NULL, &err));
