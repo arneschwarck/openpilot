67ca552cb4b44d263460a4faea6ef32c00cf8480
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2021-05-04 15:13:57 -0700

    FIX

--- selfdrive/controls/lib/turn_controller.py
+++ selfdrive/controls/lib/turn_controller.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math
-from enum import Enum
+#from enum import Enum
 from cereal import log
 from common.numpy_fast import interp
 from common.params import Params
@@ -117,10 +117,6 @@ class TurnController():
       if value == TurnControllerState.disabled:
         self._reset()
     self._state = value
-
-  @property
-  def is_active(self):
-    return self._state != TurnState.DISABLED
 
   def _reset(self):
     self._v_turn_future = 0.0
